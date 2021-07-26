2cb2e0022cb6efe7c5a667aba54c3493c8f846d0
odayushin <oda.yushin@gmail.com>
2021-07-26 20:52:14 +0900

    cors

--- lambda/app.js
+++ lambda/app.js
@@ -1,11 +1,17 @@
-const serverlessExpress = require('aws-serverless-express/middleware');
-var express = require('express');
+const serverlessExpress = require("aws-serverless-express/middleware");
+var express = require("express");
 var app = express();
+const cors = require("cors");
 
+app.use(cors());
 app.use(serverlessExpress.eventContext());
 
-app.get('/', (req, res) => {
-    res.send({message: "Hello World"});
+app.get("/", (req, res) => {
+  if (req.headers["authtoken"] != "allow") {
+    res.status(404).send({ message: "NG" });
+    return;
+  }
+  res.send({ message: "Hello World" });
 });
 
-module.exports = app
+module.exports = app;
