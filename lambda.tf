8a9bd22eb3e72873fd1f215c9472dc8e5a49cfc2
odayushin <oda.yushin@gmail.com>
2021-09-22 17:30:56 +0900

    pubishをtrue

--- lambda.tf
+++ lambda.tf
@@ -11,6 +11,7 @@ resource "aws_lambda_function" "your_function_name" {
   handler          = "index.handler"
   source_code_hash = data.archive_file.your_function_name.output_base64sha256
   runtime          = "nodejs12.x"
+  publish          = true
 
   memory_size = 128
   timeout     = 60
